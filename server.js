283628450351dd351192bd849f92b6e409dc27af
gabbistein <gabbistein@outlook.com>
2019-08-10 18:30:59 -0700

    updates to mvc structure, addition of public folder

--- server.js
+++ server.js
@@ -1,2 +1,62 @@
+// Requirements:
 var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
 
+// Middleware:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "missterious66?",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Root get route
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM tasks;", function(err, data) {
+    if (err) throw err;
+    // Stuff
+    res.render("index", { tasks: data });
+  });
+});
+
+app.post("/", function(req, res) {
+  // Test it
+  // console.log('You sent, ' + req.body.task);
+
+  // Test it
+  // return res.send('You sent, ' + req.body.task);
+
+  // When using the MySQL package, we'd use ?s in place of any values to be inserted, which are then swapped out with corresponding elements in the array
+  // This helps us avoid an exploit known as SQL injection which we'd be open to if we used string concatenation
+  // https://en.wikipedia.org/wiki/SQL_injection
+  connection.query("INSERT INTO tasks (task) VALUES (?)", [req.body.task], function(err, result) {
+    if (err) throw err;
+
+    res.redirect("/");
+  });
+});
+
+app.listen(PORT, function() {
+  console.log("Server listening on: PORT" + PORT);
+});
